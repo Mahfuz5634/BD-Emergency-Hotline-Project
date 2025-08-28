37c3504306a949c45042c47c269fe19b65510a56
Mahfuz <mahfuzru5634@gmail.com>
2025-08-28 20:26:35 +0600

    added history section and it'sfunctionalities

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ for (let CB of copyButton) {
     copycount += 1;
     document.getElementById("copy-count").innerText = copycount;
 
-    const card = CB.parentNode.parentNode; 
-    const text = card.querySelector('h1').innerText;
-    const num  = card.querySelector('h2').innerText;
+    const card = CB.parentNode.parentNode;
+    const text = card.querySelector("h1").innerText;
+    const num = card.querySelector("h2").innerText;
 
     try {
       await navigator.clipboard.writeText(`${num}`);
@@ -33,3 +33,51 @@ for (let CB of copyButton) {
     }
   });
 }
+
+// callButtonSection
+const callButton = document.getElementsByClassName("call-button");
+
+for (let CB of callButton) {
+  CB.addEventListener("click", function () {
+    let count = Number(document.getElementById("coin-count").innerText);
+
+    if (count >= 20) {
+      count -= 20;
+      document.getElementById("coin-count").innerText = count;
+
+      const card = CB.parentNode.parentNode;
+      const text = card.querySelector("h1").innerText;
+      const num = card.querySelector("h2").innerText;
+      let now = new Date();
+      let curTime = now.toLocaleTimeString();
+      alert('📞 You are calling ' + text + ': ' + num);
+
+      let newHistory = document.getElementById("history-section");
+      let addHistory = document.createElement("div");
+      addHistory.innerHTML = `<div  class="bg-[#cdcaca36] w-[98%] h-[70px] m-auto mt-2 flex justify-between items-center rounded-sm">
+               <div class="p-2">
+                 <h1 class="font-semibold text-[15px]">${text}</h1>
+                 <p>${num}</p>
+               </div>
+               <div class="p-2 text-[13px] ]">${curTime}</div>
+               
+            </div>`;
+
+      newHistory.appendChild(addHistory);
+     
+    }
+    else{
+    alert('🚫 Insufficient Balance!\nYou need at least 20 coins to make a call.');
+
+    }
+     
+
+  });
+}
+
+
+// clearbuttonsection
+document.getElementById('clearButton').addEventListener('click',function(){
+  
+  document.getElementById('history-section').innerHTML='';
+})
